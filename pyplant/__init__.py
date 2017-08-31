c34a81778b4c2d15e1b0b7b913ce2ea33ad51186
gleb <gleb@example.com>
2017-08-31 15:38:16 +0200

    Wrapped into a Python package for ease of importing into other projects.

---
+++ pyplant/__init__.py
@@ -0,0 +1,1 @@
+from pyplant.pyplant import *
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pyplant',
+      version='0.1',
+      description='Python function pipeline',
+      packages=['pyplant'],
+      zip_safe=False)
