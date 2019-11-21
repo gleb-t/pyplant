fec7e6e94709eaba5a3cd3067bf5029c74165e5a
gleb-t <gleb-t@users.noreply.github.com>
2019-11-21 17:05:42 +0100

    Preparing metadata for PyPI publication.

--- pyplant/__init__.py
+++ pyplant/__init__.py
@@ -1,7 +1,5 @@
 from pyplant.pyplant import *
 import pyplant.test as test
-# from pyplant.test.PipeworkMock import *
-# from pyplant.test.test_utils import *
 
 # A hack which hides the magic methods from PyCharm,
 # so that the missing field warnings still work.
--- setup.py
+++ setup.py
@@ -3,5 +3,21 @@ from setuptools import setup
 setup(name='pyplant',
       version='0.3.9',
       description='Python function pipeline',
+      license='MIT',
+      author='gleb-t',
+      url='https://github.com/gleb-t/pyplant',
+
       packages=['pyplant', 'pyplant.test'],
-      zip_safe=False, install_requires=['dateutils'])
+      zip_safe=False, install_requires=['dateutils', 'numpy'],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+      ],
+      )
