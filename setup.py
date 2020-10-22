030bcaa2896eca420976693c3031006c5ef35145
gleb-t <gleb-t@users.noreply.github.com>
2020-10-22 13:50:18 +0200

    Version increment.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyplant',
-      version='0.5.0',
+      version='0.5.1',
       description='Python function pipeline',
       license='MIT',
       author='gleb-t',
